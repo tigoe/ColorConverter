260d6801c360bcab317cfea5e7750dfb0187574e
tigoe <tom.igoe@gmail.com>
2018-06-27 15:06:11 -0400

    updated example

--- examples/SerialToColorConverter/SerialToColorConverter.ino
+++ examples/SerialToColorConverter/SerialToColorConverter.ino
@@ -1,35 +1,59 @@
+/*
+   Serial to color converter
+
+   This sketch lets you test the color converter. To do so, open the serial monitor
+   and send one of these strings:
+
+   h xx xx xx\n
+   A hue, saturation, intensity string that will return the corresponding RGB values
+   hue: 0-360, saturation: 0-100, intensity: 0-100
+
+   H xx xx xx\n
+   A hue, saturation, intensity string that will return the corresponding RGBW values
+   hue: 0-360, saturation: 0-100, intensity: 0-100
+
+   r h xx xx xx\n
+   A red, green, blue string that will return the corresponding HSI values
+   red: 0-255,. green: 0-255, blue: 0-255
+
+   created 26 June 2018
+   by Tom Igoe
+*/
+
 #include <ColorConverter.h>
 
 ColorConverter converter;
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.setTimeout(11);
+  Serial.begin(9600);       // initialize serial communication
+  while (!Serial);          // wait until serial monitor is opened
+  Serial.setTimeout(10);    // set timeout for parseInt() to 10 ms
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available()) {
-    char c = Serial.read();
+  if (Serial.available()) {   // if there's any serial data,
+    char c = Serial.read();   // read one byte
 
-    if (c == 'h') {
-      float h = Serial.parseInt();
+    if (c == 'h') {       // if byte was h:
+      float h = Serial.parseInt();    // read ASCII numeric strings
       float s = Serial.parseInt();
-      float l = Serial.parseInt();
-      RGBColor color = converter.HSItoRGB(h, s, l);
+      float i = Serial.parseInt();
+      // do the conversion:
+      RGBColor color = converter.HSItoRGB(h, s, i);
+      // print the results:
       Serial.print(color.red);
       Serial.print(" ");
       Serial.print(color.green);
       Serial.print(" ");
-     Serial.println(color.blue);
+      Serial.println(color.blue);
     }
 
-    if (c == 'H') {
-      float h = Serial.parseInt();
+    if (c == 'H') {       // if byte was H:
+      float h = Serial.parseInt();    // read ASCII numeric strings
       float s = Serial.parseInt();
-      float l = Serial.parseInt();
-      RGBColor color = converter.HSItoRGBW(h, s, l);
+      float i = Serial.parseInt();
+      // do the conversion:
+      RGBColor color = converter.HSItoRGBW(h, s, i);
+      // print the results:
       Serial.print(color.red);
       Serial.print(" ");
       Serial.print(color.green);
@@ -38,24 +62,19 @@ void loop() {
       Serial.print(" ");
       Serial.println(color.white);
     }
-    if (c == 'r') {
-      float r = Serial.parseInt();
+    if (c == 'r') {      // if byte was r:
+      float r = Serial.parseInt();    // read ASCII numeric strings
       float g = Serial.parseInt();
       float b = Serial.parseInt();
+      // do the conversion:
       HSIColor color = converter.RGBtoHSI(r, g, b);
-
-      Serial.print(r);
-      Serial.print(" ");
-      Serial.print(g);
-      Serial.print(" ");
-      Serial.println(b);
-
+      // print the results:
       Serial.print(color.hue);
       Serial.print(" ");
       Serial.print(color.saturation);
       Serial.print(" ");
       Serial.println(color.intensity);
     }
-    Serial.println();
+    Serial.println(); // add a blank line
   }
 }
